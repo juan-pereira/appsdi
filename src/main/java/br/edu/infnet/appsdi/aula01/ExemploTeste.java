6dfdfcdae7c811936a20ffa4541e1bc44c252552
juan-pereira <53612511+juan-pereira@users.noreply.github.com>
2021-01-29 00:37:07 -0300

    added more bugs to fix later

---
+++ src/main/java/br/edu/infnet/appsdi/aula01/ExemploTeste.java
@@ -0,0 +1,7 @@
+package br.edu.infnet.appsdi.aula01;
+
+public class ExemploTeste {
+    public static void main(String[] args) {
+        System.out.println("Juan é o mais brabo em Java Language");
+    }
+}
